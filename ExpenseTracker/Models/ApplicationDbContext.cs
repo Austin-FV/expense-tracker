39ed745aca1944f159f4caad4e7c6b04883f7c9b
Austin-FV <austin.varghese7@gmail.com>
2024-01-15 17:31:35 -0500

    initial commit, initialized ASP.NET Core MVC web app and setup DB Tables for Transactions in local MS SQL server

---
+++ ExpenseTracker/Models/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ExpenseTracker.Models
+{
+    public class ApplicationDbContext:DbContext
+    {
+        // the context will be created through dependency injection
+        // pass the db provider (mysql/sql server)
+        public ApplicationDbContext(DbContextOptions options):base(options)
+        {
+
+        }
+
+        // setting db table column names used
+        public DbSet<Transaction> Transactions { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
